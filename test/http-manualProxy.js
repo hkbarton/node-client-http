6099600845aa0c692503087e4bd922e689698bc2
FXHibon <francois-xavier.hibon@capgemini.com>
2015-07-22 14:24:13 +0200

    Add support to any response format (binary etc ...)

--- test/http-manualProxy.js
+++ test/http-manualProxy.js
@@ -2,6 +2,6 @@ var http = require('../index.js');
 
 http.setProxy('http://127.0.0.1:8081');
 http.get("http://www.google.com/", function(data, err){
-    data && console.log(data);
+    data && console.log(data.toString());
     err && console.log(err);
 });
--- test/http-post.js
+++ test/http-post.js
@@ -1,6 +1,6 @@
 var http = require('../index.js');
 
 http.request("http://httpbin.org/post", function(data, err){
-    data && console.log(data);
+    data && console.log(data.toString());
     err && console.log(err);
 }, "fname=hello&lname=world", {"Content-Type":"application/x-www-form-urlencoded"});
